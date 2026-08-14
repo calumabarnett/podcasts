b67167d96ba2db300e9d4172c480a1a27236ec32
Calum Barnett <calum.barnett@outlook.com>
2026-08-14 23:40:21 +0100

    Update ignored files

--- shared/build/buildkonfig/commonMain/com/podcasts/shared/BuildKonfig.kt
+++
@@ -1,9 +0,0 @@
-package com.podcasts.shared
-
-import kotlin.String
-
-internal object BuildKonfig {
-  public val PODCAST_INDEX_API_KEY: String = ""
-
-  public val PODCAST_INDEX_API_SECRET: String = ""
-}
